ec36af6d1758fef5b22ac3d0a70fccb33ae2e45b
Vlad-Costin Panainte <vpanainte@griddynamics.com>
2024-12-10 14:54:37 +0200

    add helper scripts for infrastructure provisioning and configuration

---
+++ scripts/0_setup_terraform_remote_backend.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -euo pipefail
+
+echo "-> Setting up GCP Cloud Storage bucket for Terraform remote backend"
+terraform -chdir=../terraform/terraform_remote_state apply --auto-approve
