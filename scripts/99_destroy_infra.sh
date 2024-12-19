cb4f6067528e7e9390ebd55cee731d474ee768fc
Vlad-Costin Panainte <vpanainte@griddynamics.com>
2024-12-19 18:23:01 +0200

    add spring_petclinic_infrastructure destroy command to destroy_infra script

--- scripts/99_destroy_infra.sh
+++ scripts/99_destroy_infra.sh
@@ -2,6 +2,7 @@
 set -euo pipefail
 
 echo '-> Destroying infrastructure'
+terraform -chdir=../terraform/spring_petclinic_infrastructure destroy --auto-approve
 terraform -chdir=../terraform/jenkins_master_and_nodes destroy --auto-approve
 
 # echo '-> Removing GCP Storage bucket used for Terraform remote state'
